4ac7a3e4a409896bad829fdec853c6b8047d9ae9
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-09-08 03:57:19 +1000

    Create zero.js

---
+++ Edabit Answer - Number less than Zero or Equal to/zero.js
@@ -0,0 +1,10 @@
+// Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+
+// Examples
+// lessThanOrEqualToZero(5) ➞ false
+
+// lessThanOrEqualToZero(0) ➞ true
+
+// lessThanOrEqualToZero(-2) ➞ true
+
+const lessThanOrEqualToZero = (num) => (num <= 0) ? true : false;
