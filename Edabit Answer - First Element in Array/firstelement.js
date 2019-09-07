19bd663d3f66c42f9fbfd28a9e50c5f6396586da
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-09-08 04:25:54 +1000

    Create firstelement.js

---
+++ Edabit Answer - First Element in Array/firstelement.js
@@ -0,0 +1,12 @@
+// Create a function that takes an array and returns the first element.
+
+// Examples
+// getFirstValue([1, 2, 3]) ➞ 1
+
+// getFirstValue([80, 5, 100]) ➞ 80
+
+// getFirstValue([-500, 0, 50]) ➞ -500
+// Notes
+// The first element in an array always has an index of 0.
+
+const getFirstValue = arr => arr[0];
