286361589b8c36c102e38ac3910669a4f648bc11
Chantel <35864046+Chantelsky@users.noreply.github.com>
2019-09-08 21:24:16 +1000

    Create dashes.js

---
+++ Edabit Answer - Convert Number to str dashes/dashes.js
@@ -0,0 +1,13 @@
+// Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+
+// Examples
+// Go(1) ➞ "-"
+
+// Go(5) ➞ "-----"
+
+// Go(3) ➞ "---"
+// Notes
+// You will be provided integers ranging from 1 to 60.
+// Don't forget to return your result as a string.
+
+const Go = num => '-'.repeat(num)
